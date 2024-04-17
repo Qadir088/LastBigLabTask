c7f0c4cf38025f411bd7e86df36b43aca9a0606f
Qadir088 <qadircabbarli55@gmail.com>
2024-04-17 18:21:14 +0400

    task solved

---
+++ LabBiggestTask/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabBiggestTask;
+
+namespace Lab24
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public Payment payment;
+        public double CashBalance { get; set; }
+        public double CardBalance { get; set; }
+        public List<Product> Basket { get; } = new List<Product>();
+
+        public Customer()
+        {
+
+        }
+
+
+
+    }
+}
